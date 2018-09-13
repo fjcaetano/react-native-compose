8a9051904b46c73e6fd0357174c2476568461856
Flávio Caetano <caetano@work.co>
2018-09-13 16:04:50 -0300

    Defining JS objects in index

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ import React from 'react';
 
 import { NativeModules } from 'react-native';
 
-export const ComposeMessage = NativeModules.FJCMessageCompose;
-export const ComposeMail = NativeModules.FJCMailCompose;
+const FJCComposeMessage = NativeModules.FJCMessageCompose;
+const FJCComposeMail = NativeModules.FJCMailCompose;
 
 export const ComposeResult = {
   sent: 'sent',
   saved: 'saved',
+};
+
+export const ComposeMessage = {
+  canSendText() {
+    return FJCComposeMessage.canSendText();
+  },
+
+  canSendAttachments() {
+    return FJCComposeMessage.canSendAttachments();
+  },
+
+  canSendSubject() {
+    return FJCComposeMessage.canSendSubject();
+  },
+
+  send(body) {
+    return FJCComposeMessage.send(body);
+  },
+};
+
+export const ComposeMail = {
+  canSendMail() {
+    return FJCComposeMail.canSendMail();
+  },
+
+  send(body) {
+    return FJCComposeMail.send(body);
+  },
 };
